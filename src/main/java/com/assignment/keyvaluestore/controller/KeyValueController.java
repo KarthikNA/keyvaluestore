c4e95c14f3d914f7b9049901ebbe7678e22523dd
Karthik N A <karthik.na@tapzo.com>
2018-10-14 14:57:26 +0530

    added a key value dto to take input

--- src/main/java/com/assignment/keyvaluestore/controller/KeyValueController.java
+++ src/main/java/com/assignment/keyvaluestore/controller/KeyValueController.java
@@ -1,9 +1,12 @@
 package com.assignment.keyvaluestore.controller;
 
+import com.assignment.keyvaluestore.dto.KeyValueDto;
 import java.util.HashMap;
 import java.util.Map;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,14 +16,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class KeyValueController {
 
   @RequestMapping(value = "/key/{key}", method = RequestMethod.GET)
-  public ResponseEntity<Map<String, String>> getValue() {
+  public ResponseEntity<Map<String, String>> getValue(@PathVariable(name = "key") String key) {
     Map<String, String> response = new HashMap<>();
     // todo - get data corresponding to key
     return new ResponseEntity(response, HttpStatus.OK);
   }
 
-  @RequestMapping(value = "/key/{key}", method = RequestMethod.PUT)
-  public ResponseEntity<Map<String, String>> putValue() {
+  @RequestMapping(value = "/key", method = RequestMethod.PUT)
+  public ResponseEntity<Map<String, String>> putValue(@RequestBody KeyValueDto dto) {
     Map<String, String> response = new HashMap<>();
     // todo - update key if already exists, else insert newly
     return new ResponseEntity(response, HttpStatus.OK);
