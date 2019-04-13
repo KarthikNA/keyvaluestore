c4a34863ce0e09c939b35678bf256cfe96b324df
Karthik N A <karthik.na@tapzo.com>
2019-04-13 12:08:56 +0530

    controller changes

--- src/main/java/com/assignment/keyvaluestore/udaan/controller/UserController.java
+++ src/main/java/com/assignment/keyvaluestore/udaan/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.assignment.keyvaluestore.udaan.controller;
 
+import com.assignment.keyvaluestore.udaan.model.PendingAmount;
 import com.assignment.keyvaluestore.udaan.model.User;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,7 +21,14 @@ public class UserController {
 
     @RequestMapping(value = "", method = RequestMethod.POST)
     public ResponseEntity<User> createUser(@RequestBody User user) {
+        // creates new user
         return new ResponseEntity<User>(HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "", method = RequestMethod.POST)
+    public ResponseEntity<PendingAmount> getUserPendingAmount(Integer id) {
+        // returns the amount the person owes and how much others owe
+        return new ResponseEntity<PendingAmount>(HttpStatus.OK);
     }
 
 
